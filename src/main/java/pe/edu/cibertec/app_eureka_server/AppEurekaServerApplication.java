06b4099f87347343551a9d668cff267cb9af4325
ProgramandoWeb2 <daplicacionesmoviles1@gmail.com>
2024-09-29 17:26:57 -0500

    ejercicio terminado

---
+++ src/main/java/pe/edu/cibertec/app_eureka_server/AppEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package pe.edu.cibertec.app_eureka_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class AppEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEurekaServerApplication.class, args);
+	}
+
+}
